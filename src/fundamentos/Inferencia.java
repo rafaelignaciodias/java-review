2c488079ecc3b398957f8278a2f435c38b0f5446
Rafael Ignacio Dias <rafaelignaciodias@gmail.com>
2024-08-02 19:23:12 -0400

    Dia 001

---
+++ src/fundamentos/Inferencia.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+
+		double a = 4.5;
+		System.out.println(a);
+
+		var b = 4.5;
+		System.out.println(b);
+
+		var c = "Texto";
+		System.out.println(c);
+
+		c = "Outro Texto";
+		System.out.println(c);
+
+		double d;
+		d = 123.45;
+		System.out.println(d);
+	}
+}
